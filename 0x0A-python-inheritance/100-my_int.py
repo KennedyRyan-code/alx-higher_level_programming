d8d7da1afe951d39ee5cfb25237d24d8b1758bb9
KennedyRyan-code <kenryan49@gmail.com>
2023-07-10 20:25:24 +0300

    My integer

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Write a class MyInt that inherits from int."""
+
+
+class MyInt(int):
+    def __eq__(self, other):
+        """
+        Inverted equality comparison.
+
+        Args:
+            other: The object to compare against.
+
+        Returns:
+            True if the values are not equal; False otherwise.
+        """
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """
+        Inverted inequality comparison.
+
+        Args:
+            other: The object to compare against.
+
+        Returns:
+            True if the values are equal; False otherwise.
+        """
+        return super().__eq__(other)
