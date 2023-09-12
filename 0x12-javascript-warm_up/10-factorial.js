276f83a56b26f787cc00dad580a1ae655c8d0653
KennedyRyan-code <kenryan49@gmail.com>
2023-09-12 09:08:08 +0300

    Factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+function factorial (n) {
+  if (isNaN(n)) {
+    return 1; // Factorial of NaN is 1
+  } else if (n <= 1) {
+    return 1; // Factorial of 0 and 1 is 1
+  } else {
+    return n * factorial(n - 1); // Recursive call to compute the factorial
+  }
+}
+
+const arg = parseInt(process.argv[2]);
+
+const result = factorial(arg);
+console.log(result);
