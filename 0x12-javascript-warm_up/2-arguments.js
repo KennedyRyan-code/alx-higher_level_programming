a2441a83984682c79f1ac962b91d149bec3358ce
KennedyRyan-code <kenryan49@gmail.com>
2023-09-12 07:42:22 +0300

    Process Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const argc = process.argv.slice(2);
+
+if (argc.length === 0) {
+  console.log('No argument');
+} else if (argc.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Argument found');
+}
