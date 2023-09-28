8771fa6b08585d48862ce23e0347e06a7c707c05
KennedyRyan-code <kenryan49@gmail.com>
2023-09-28 18:59:12 +0300

    cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # URL as the first argument to the script
+# Send a request to the URL and display the size of the body of the response
 url=$1
 
-# Send a request to the URL and display the size of the body of the response
-curl -sI $url | grep -i "Content-Length" | awk '{print $2}'
+curl -sI $url | awk '/Content-Length/{print $2}'
