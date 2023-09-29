070d1d01f0f33e848e27178b169680b34a9555e7
KennedyRyan-code <kenryan49@gmail.com>
2023-09-29 23:20:28 +0300

    Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+    sends a POST request to http://0.0.0.0:5000/search_user
+    with the letter as a parameter.
+"""
+
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+    else:
+        q = ""
+
+    response = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+
+    try:
+        json_response = response.json()
+        if json_response:
+            print("[{}] {}".format(json_response.get('id'),
+                  json_response.get('name')))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
